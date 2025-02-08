76813a62028508e170e1ce927b5ca467f097764b
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-02-08 13:20:23 +0530

    added dev dependencies for Prettier and Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierConfig,
+  {
+    rules: {
+      ...eslintPluginPrettier.configs.recommended.rules, // Enable Prettier as an ESLint rule
+    },
+    plugins: ["react", "@typescript-eslint", "prettier"],
+  },
+];
